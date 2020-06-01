9384b76ba06a9e51931465fa0a19d0cb4712d50e
Antonio Junior <jr.sistema@gmail.com>
2020-06-01 11:55:33 -0300

    layout do jogo

--- src/main.js
+++ src/main.js
@@ -15,9 +15,6 @@ var exemplo = new Vue({
   el: "#app",
   data: {
     titulo: "Campenato Brasileiro Série - A",
-    bool: true,
-    numeroInteiro: 10,
-    numeroFloat: 10.5,
 
     times: [
       new Time("Palmeiras", require("./assets/palmeiras_60x60.png")),
@@ -41,13 +38,30 @@ var exemplo = new Vue({
       new Time("Santa Cruz", require("./assets/santa_cruz_60x60.png")),
       new Time("América-MG", require("./assets/america_mg_60x60.png"))
     ],
-    alfabero: {
-      a: "A",
-      b: "B",
-      c: "C",
-      d: "D",
-      e: "E"
+
+    novoJogo: {
+      casa : {
+        time: null,
+        gols: 0
+      },
+      fora : {
+        time: null,
+        gols: 0
+      }
     }
-  }
+  },
+
+
+    created(){
+      let indexCasa = Math.floor(Math.random() * 20),
+          indexFora = Math.floor(Math.random() * 20);
+          console.log(indexCasa);
+      this.novoJogo.casa.time = this.times[indexCasa];
+      this.novoJogo.casa.gols = 0;
+      this.novoJogo.fora.time = this.times[indexFora];
+      this.novoJogo.fora.gols = 0;
+    }
+
+
   //render: h => h(App)
 });
